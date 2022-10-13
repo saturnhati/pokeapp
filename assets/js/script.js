11d963769176d5ac7e3a2219f33ad7bd2b88caca
saturnhati <76071008+saturnhati@users.noreply.github.com>
2022-10-13 14:16:30 +0200

    smtn (?)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
-function fetchKantoPokemon() {
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
-        .then(response => response.json())
-        .then(allpokemon => console.log(allpokemon))
-}
+let pokemons;
+
+fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151').then(
+    (httpResponse) => {
+        console.log("Fine della richiesta, ecco la risposta HTTP")
+        console.log(httpResponse)
+
+        httpResponse.json().then((responseText) => {
+            console.log(responseText)
+        })
+    })
