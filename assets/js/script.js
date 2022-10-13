9a46b237070371761f8d8987eb93abfa4c57a5e3
saturnhati <76071008+saturnhati@users.noreply.github.com>
2022-10-13 11:20:20 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+function fetchKantoPokemon() {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+        .then(response => response.json())
+        .then(allpokemon => console.log(allpokemon))
+}
